cfa184609df81910d3c6d8dabee7921e1b0ca125
Akshat-Bharara <akshat.bharara05@gmail.com>
2024-03-17 23:36:54 +0530

    added methods

--- csvizard/methods.py
+++ csvizard/methods.py
@@ -1,14 +1,59 @@
-def filter_csv(filepath) :
-  pass
+import csv
+import pandas as pd
+
+
+'''
+Example:
+criteria = {'age': '25', 'gender': 'Male'}
+filtered_data = filter_csv('example.csv', criteria)
+'''
+
+def filter_csv(filepath, criteria):
+    filtered_rows = []
+    with open(filepath, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if all(row[column] == value for column, value in criteria.items()):
+                filtered_rows.append(row)
+    return filtered_rows
 
 def join_csv(*filepaths) :
-  pass
+  csv_files=[]
 
-def aggregate_csv(filepath, col, opr) :
-  pass
+  for i in filepaths:
+     csv_files.append(i)
+
+  dfs = [pd.read_csv(file) for file in csv_files]
+
+  merged_df = dfs[0]
+
+  for df in dfs[1:]:
+    merged_df = pd.merge(merged_df, df, on='common_column', how='inner')
+
+  return merged_df
+     
+
+def aggregate_csv(filepath, col, opr):
+    
+    df = pd.read_csv(filepath)
+    
+    if opr == 'sum':
+        result = df[col].sum()
+    elif opr == 'mean':
+        result = df[col].mean()
+    elif opr == 'min':
+        result = df[col].min()
+    elif opr == 'max':
+        result = df[col].max()
+    return result
+    
+
 
 def format_csv(filepath) :
   pass
 
 def get_csv_cols(filepath) :
-  pass
+  with open(filepath, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        column_names = next(reader)
+  return column_names
